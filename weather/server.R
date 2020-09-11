c88d122df832317b2cd6ca08447eb4dcbded3858
samsoe <esamsoe@gmail.com>
2020-09-11 09:25:50 -0600

    Initial Commit

---
+++ weather/server.R
@@ -0,0 +1,76 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(bigrquery)
+library(DBI)
+library(plotly)
+
+# Define server logic required to draw a histogram
+function(input, output) {
+  observeEvent(
+    input$show_about, {
+      showModal(modalDialog("Place descriptive text in this section.", title = 'About'))
+    })
+  
+  # query BigQuery
+  rval_bq <- reactive({
+    billing <-Sys.getenv("BIGQUERY_TEST_PROJECT")
+    token_path <- Sys.getenv("TOKEN_PATH")
+    bq_auth(path = "./mpg-data-warehouse-c5cd05731d19.json")
+    
+    project = "mpg-data-warehouse"
+    dataset = "weather_summaries"
+    
+    con <- dbConnect(
+      bigrquery::bigquery(),
+      project = project,
+      dataset = dataset,
+      billing = billing
+    )
+    
+    # weather <- tbl(con, "mpg_ranch_daily")
+    sql <- "SELECT * FROM `mpg-data-warehouse.weather_summaries.mpg_ranch_daily`"
+    weather_db <- dbGetQuery(con, sql)
+    
+    # weather_db
+    # weather <- dbGetQuery(con, sql)
+  })
+  
+  # perform Filters
+  rval_data <- reactive({
+    rval_bq() %>%
+      filter(date_day >= input$date_range[1] &
+               date_day <= input$date_range[2] &
+               station %in% input$station)
+    
+  })
+  
+  output$linePlot <- renderPlot({
+    rval_data() %>%
+      # filter(station %in% input$station) %>%
+      ggplot(aes(x=date_day, y=temp_F_mean)) +
+      geom_line(aes(color = station))
+  })
+  
+  output$weather_table <- DT::renderDT({
+    rval_data() %>% 
+      # filter(station %in% input$station) %>%
+      select(date_day, station, temp_F_mean, temp_F_max, temp_F_min)
+    })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('mpg_weather-', Sys.Date(), '.csv', sep='')
+    },
+    content = function(con) {
+      write.csv(rval_data(), con)
+    }
+  )
+  
+  observeEvent(input$about, {
+    showModal(modalDialog(
+      title = "About",
+      "Further documentation can be found here."
+    ))
+  })
+}
---
+++ weather/ui.R
@@ -0,0 +1,49 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+fluidPage(
+  
+  # Application title
+  titlePanel("MPG Ranch Weather"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      dateRangeInput("date_range", 
+                     label = "Date range",
+                     start = "2019-01-01",
+                     end = "2019-12-31"),
+      selectInput("station",
+                  "Select station:",
+                  choices = c("baldy draw", "baldy summit", "indian ridge",
+                              "orchard house", "sainfoin bench", "south baldy ridge"),
+                  selected = "orchard house",
+                  multiple = TRUE),
+      downloadButton("downloadData", "Download"),
+      actionButton("about", "About")
+      # selectInput("variables",
+      #             "Select variables:",
+      #             choices = c("Temperature", "Precipitation", "Wind"),
+      #             selected = "Temperature",
+      #             multiple = TRUE),
+    ),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", plotOutput("linePlot")),
+                  tabPanel("Table", DT::DTOutput("weather_table"))
+                  # tabPanel("About", textOutput("about"))
+      )
+    )
+  )
+)
